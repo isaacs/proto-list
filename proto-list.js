ad1a9d606b88e6fde3789beb5e6aae60388bcce9
isaacs <i@izs.me>
2012-08-13 16:28:34 -0700

    Move tests into test/basic.js

--- proto-list.js
+++ proto-list.js
@@ -56,39 +56,3 @@ ProtoList.prototype =
       return this.list.splice.apply(this.list, arguments)
     }
   }
-
-if (module === require.main) {
-
-var tap = require("tap")
-  , test = tap.test
-
-tap.plan(1)
-
-tap.test("protoList tests", function (t) {
-  var p = new ProtoList
-  p.push({foo:"bar"})
-  p.push({})
-  p.set("foo", "baz")
-  t.equal(p.get("foo"), "baz")
-
-  var p = new ProtoList
-  p.push({foo:"bar"})
-  p.set("foo", "baz")
-  t.equal(p.get("foo"), "baz")
-  t.equal(p.length, 1)
-  p.pop()
-  t.equal(p.length, 0)
-  p.set("foo", "asdf")
-  t.equal(p.length, 1)
-  t.equal(p.get("foo"), "asdf")
-  p.push({bar:"baz"})
-  t.equal(p.length, 2)
-  t.equal(p.get("foo"), "asdf")
-  p.shift()
-  t.equal(p.length, 1)
-  t.equal(p.get("foo"), undefined)
-  t.end()
-})
-
-
-}
